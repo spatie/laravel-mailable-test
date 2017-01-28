4ab497cc5313bdace0682edee7be6b2ec15083d8
freek <freek@spatie.be>
2017-01-28 10:35:44 +0100

    commit

---
+++ tests/Mailable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Spatie\MailableTest\Test;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+abstract class BaseMailable extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $parameters = [];
+
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->to('client@mail.com')
+            ->replyTo('replyto@mail.com')
+            ->cc('cc@mail.com')
+            ->bcc('bcc@mail.com')
+            ->subject('test mailable')
+            ->markdown('mail.dummy');
+    }
+}
